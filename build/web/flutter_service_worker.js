7ef609fb52ddf60ab507b1406c475e140b81151f
decimalist <129120814+realdecimalist@users.noreply.github.com>
2025-07-16 14:20:40 -0600

    Fix chat history loading - messages now properly display when selecting a chat session

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -36,7 +36,7 @@ const RESOURCES = {"apple-touch-icon-120x120.png": "c15f0a70a483109d9cbb938ac1a8
 "favicon.ico": "c15f0a70a483109d9cbb938ac1a8349c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "8f44f86e7de2f0328ff6abf55a08d04c",
+"flutter_bootstrap.js": "df1e44d8828a3c3b8010e0024e27249d",
 "icon-256.png": "c15f0a70a483109d9cbb938ac1a8349c",
 "icons/bluelight-144.png": "c15f0a70a483109d9cbb938ac1a8349c",
 "icons/bluelight-192.png": "554be9855b25d09948aa7cd3cbeb582d",
@@ -44,7 +44,7 @@ const RESOURCES = {"apple-touch-icon-120x120.png": "c15f0a70a483109d9cbb938ac1a8
 "icons/bluelight-72.png": "c15f0a70a483109d9cbb938ac1a8349c",
 "index.html": "ec497df239ccd7bff7e8f7c02975c5fb",
 "/": "ec497df239ccd7bff7e8f7c02975c5fb",
-"main.dart.js": "3d07af731c085f0c2decebd788b05c83",
+"main.dart.js": "a835aeb1060000aae350304e61dd9442",
 "manifest.json": "e6caee3495c2ee10db899519d2eca1f7",
 "version.json": "322a2acfc84799993e860b5b5abb3c90"};
 // The application shell files that are downloaded before a service worker can
